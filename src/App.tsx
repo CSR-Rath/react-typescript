367d3b637b7056ad9e400d5cfdd17fb68d65863a
Chhan Sophearath <sophearath.chhan@finig.asia>
2026-02-04 11:03:30 +0700

    build success

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AppRoutes from "./routes/AppRoutes";
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import AppRoutes from "./routes/AppRoutes";
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return <AppRoutes />;
+// }
+
+// export default App
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <AppRoutes />;
+  return (
+    <div>
+      Hello React + TypeScript
+    </div>
+  );
 }
 
-export default App
+export default App;
