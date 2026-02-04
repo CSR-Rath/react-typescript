b5c4a096c4ece0064c976394370c58eaf5220707
Chhan Sophearath <sophearath.chhan@finig.asia>
2026-02-04 11:51:45 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
-// import AppRoutes from "./routes/AppRoutes";
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return <AppRoutes />;
-// }
-
-// export default App
+import './App.css'
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <div>
-      Hello React + TypeScript
-    </div>
-  );
+  // const [count, setCount] = useState(0)
+
+  return <AppRoutes />;
 }
 
-export default App;
+export default App
